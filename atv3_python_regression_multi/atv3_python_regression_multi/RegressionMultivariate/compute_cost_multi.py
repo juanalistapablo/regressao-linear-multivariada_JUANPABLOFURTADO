7d24a8e949f4585cd7bdf0c01995e2b257779fa1
JUANPABLOMONDEGO <juanmondego208@gmail.com>
2025-05-03 11:44:45 -0300

    Update compute_cost_multi.py
    código incompleto da função

--- atv3_python_regression_multi/atv3_python_regression_multi/RegressionMultivariate/compute_cost_multi.py
+++ atv3_python_regression_multi/atv3_python_regression_multi/RegressionMultivariate/compute_cost_multi.py
@@ -1,17 +1,17 @@
 # Functions/compute_cost_multi.py
-"""
+
 @file compute_cost_multi.py
 @brief Computes the cost for multivariate linear regression.
 @details Este módulo contém uma função para calcular o custo de um modelo de regressão linear
           multivariada utilizando a função de custo de erro quadrático médio.
 @author Your Name <your.email@example.com>
-"""
+
 
 import numpy as np
 
 
 def compute_cost_multi(X, y, theta):
-    """
+    
     Calcula o custo para regressão linear multivariada.
 
     A função de custo é definida como:
@@ -21,16 +21,16 @@ def compute_cost_multi(X, y, theta):
     :param (ndarray) y: Vetor de valores alvo (shape: m,).
     :param (ndarray) theta: Vetor de parâmetros (shape: n+1,).
     :return (float): Valor do custo calculado.
-    """
+    
     # get the number of training examples
-    m = y.size
+    m = 
     # compute the predictions using the linear model by formula h(θ) = X @ θ
     # where @ is the matrix multiplication operator
-    predictions = X @ theta
+    predictions = 
     # compute the error vector between predictions and actual values
     # The error is the difference between the predicted values and the actual values
     # errors = predictions - y
-    errors =  predictions - y
+    errors = 
     # compute the cost as the mean squared error cost function using the formula in the docstring
-    cost = (1 / (2 * m)) * np.dot(errors.T, errors)
+    cost = 
     return cost
