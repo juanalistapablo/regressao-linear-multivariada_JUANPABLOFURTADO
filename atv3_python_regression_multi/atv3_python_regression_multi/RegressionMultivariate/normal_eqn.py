878484390c5ac68b4e5add4bfea2ceb61c9184dc
JUANPABLOMONDEGO <juanmondego208@gmail.com>
2025-05-03 11:48:30 -0300

    Update normal_eqn.py

--- atv3_python_regression_multi/atv3_python_regression_multi/RegressionMultivariate/normal_eqn.py
+++ atv3_python_regression_multi/atv3_python_regression_multi/RegressionMultivariate/normal_eqn.py
@@ -1,17 +1,17 @@
 # Functions/normal_eqn.py
-"""
+
 @file normal_eqn.py
 @brief Calcula os parâmetros θ usando a Equação Normal.
 @details Este módulo contém uma função para calcular os parâmetros de um modelo
           de regressão linear utilizando a equação normal.
 @author Your Name <your.email@example.com>
-"""
+
 
 import numpy as np
 
 
 def normal_eqn(X, y):
-    """
+    
     Resolve os parâmetros θ utilizando a equação normal.
 
     A equação normal é definida como:
@@ -21,7 +21,7 @@ def normal_eqn(X, y):
                         e cada coluna é uma feature (shape: m × n+1).
     :param (ndarray) y: Vetor de valores alvo (shape: m,).
     :return (ndarray): Vetor de parâmetros θ (shape: n+1,).
-    """
+    
     # Calcula os parâmetros θ utilizando a equação normal
     # A equação normal é uma solução fechada para o problema de regressão linear
     # que minimiza a soma dos erros quadráticos entre as previsões e os valores reais
